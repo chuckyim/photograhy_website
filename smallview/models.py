cd7b702faa97110e909d73aeda922f6af4c28ddd
Cheuk On Yim <cheukon@Cheuks-MBP-2.attlocal.net>
2020-11-25 14:29:45 -0800

    add smallview model

--- smallview/models.py
+++ smallview/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):  #models.Model allows model store data to database
+    image = models.ImageField(upload_to='images/')  #see jango doc for more model field reference and parameters
+    summary = models.CharField(max_length=200)
