93c474277a1340336a54358569529039104f59a6
Cheuk On Yim <cheukon@Cheuks-MBP-2.attlocal.net>
2020-11-28 21:16:30 -0800

    Added bootstrap and homepage

--- foto_blog/settings.py
+++ foto_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'smallview.apps.SmallviewConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- smallview/views.py
+++ smallview/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Album
 
-# Create your views here.
+def home(request):
+    albums = Album.objects
+    return render(request, 'smallview/home.html', {'albums':albums})
