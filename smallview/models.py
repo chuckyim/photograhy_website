5bc8ab49199dc1feefa22054955c26af485d13f1
Cheuk On Yim <cheukon@Cheuks-MBP-2.attlocal.net>
2021-02-02 16:53:10 -0800

    polished home page, allblogs and took out static test

--- smallview/models.py
+++ smallview/models.py
@@ -4,3 +4,4 @@ class Album(models.Model):  #models.Model allows model store data to database
     image = models.ImageField(upload_to='images/')  #see jango doc for more model field reference and parameters
     summary = models.CharField(max_length=200)
     blog_key = models.IntegerField(default=999)
+    feature_key = models.IntegerField(default=999)
--- smallview/views.py
+++ smallview/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 def home(request):
-    albums = Album.objects
-    return render(request, 'smallview/home.html', {'albums':albums})
+    
+    first  = get_object_or_404(Album, feature_key=1)
+    second = get_object_or_404(Album, feature_key=2)
+    third  = get_object_or_404(Album, feature_key=3)
+    fourth = get_object_or_404(Album, feature_key=4)
+    fifth  = get_object_or_404(Album, feature_key=5)
+    sixth  = get_object_or_404(Album, feature_key=6)
+    # sixth   = Album.objects.filter(feature_key=6)
+    
+    return render(request, 'smallview/home.html', {
+       'first':first,
+       'second':second,
+       'third':third,
+       'fourth':fourth,
+       'fifth':fifth,
+       'sixth':sixth
+        })
